ac3f6235cdfef1c3af78ea6c5ed47c46dd8d0c7c
Ray Peng <pengray2015@gmail.com>
2019-06-10 19:09:04 -0700

    DebF4

--- Chapter05/DebuggingExercises/DebugFive4.java
+++ Chapter05/DebuggingExercises/DebugFive4.java
@@ -1,32 +1,33 @@
 // DebugFive4.java
 // Outputs highest of four numbers
-import javax.swing.*;
+//corrected:
+
+package debug;
+import javax.swing.JOptionPane;
 public class DebugFive4
 {
-   public static void main (String args[]) 
+   public static void main (String args[])
    {
       int one, two, three, four;
       String str, output;
       str = JOptionPane.showInputDialog(null,"Enter an integer");
-      one = Integer.parseInt();
+      one = Integer.parseInt(str);
       str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
+      two = Integer.parseInt(str);
       str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
+      three = Integer.parseInt(str);
       str = JOptionPane.showInputDialog(null,"Enter an integer");
-      four = Integer.parseInt();
-      if(one > two & one >= three && one < four)
+      four = Integer.parseInt(str);
+      if(one > two & one > three && one > four)
          output = "Highest is " + one;
       else
-         if(two > one || two > three && two > four)
+         if(two > one && two > three && two > four)
             output = "Highest is " + two;
          else
-           if(three > one && three > two || three == four)
+           if(three > one && three > two && three > four)
               output = "Highest is " + three;
            else
-              output = "Highest is " + three;
+              output = "Highest is " + four;
       JOptionPane.showMessageDialog(null, output);
    }
 }
-
-
