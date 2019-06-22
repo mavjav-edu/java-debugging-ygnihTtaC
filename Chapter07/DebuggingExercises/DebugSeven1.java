545eb59a9cac514fad915471d710c3792abc56f9
Ray Peng <pengray2015@gmail.com>
2019-06-22 14:03:45 -0700

    I didnt know how to do 2 and 4

--- Chapter07/DebuggingExercises/DebugSeven1.java
+++ Chapter07/DebuggingExercises/DebugSeven1.java
@@ -7,15 +7,30 @@ public class DebugSeven1
       String name2 = "Roger";
       String name3 = "Stacy";
 
-      if(name1== name2)
+      if(name1.equals(name2)){
         System.out.println(name1 + " and " + name2 +
           " are the same");
-      if(name1 == name3)
+      }
+      else{
+        System.out.println(name1 + " and " + name2 + " are not the same");
+      }
+      if(name1.equals(name3)){
         System.out.println(name1 + " and " + name3 +
           " are the same");
-      if(name1 == "roger")
+      }
+      else{
+        System.out.println(name1 + " and " + name3 + " are not the same");
+      }
+      if(name1 == "roger"){
         System.out.println(name1 + " and 'roger' are the same");
-      if(name1 == "Roger")
+      }
+      else
+        System.out.println(name1 + " and 'roger' are not the same");
+      if(name1 == "Roger"){
         System.out.println(name1 + " and 'Roger' are the same");
+      }
+      else{
+        System.out.println(name1 + " and 'roger' are not the same");
+      }
    }
 }
