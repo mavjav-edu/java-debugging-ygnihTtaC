76929bdc21345614c2b45d587b7f3b01920c8c49
Ray Peng <pengray2015@gmail.com>
2019-06-25 17:59:37 -0700

    Update DebugSeven4.java
    Did 4 forgot to commit to master

--- Chapter07/DebuggingExercises/DebugSeven4.java
+++ Chapter07/DebuggingExercises/DebugSeven4.java
@@ -1,4 +1,4 @@
-// Converts a string to lowercase, and 
+// Converts a string to lowercase, and
 // displays the string's length
 // as well as a count of letters
 public class DebugSeven4
@@ -7,18 +7,17 @@ public class DebugSeven4
    {
       String aString = "HELP!! I need to get 37 things DONE today!!";
       int numLetters = 0;
-      int stringLength = aString.length;
+      int stringLength = aString.length();
       System.out.println("In all lowercase, the sentence is: ");
-      for(int i = 0; i < length; i++)
+      for(int i = 0; i < stringLength; i++)
       {
-         char ch = Character.toLowerCase(aString.charAt(length));
+         char ch = Character.toLowerCase(aString.charAt(i));
          System.out.print(ch);
-         if(Character.isLetter())
+         if(Character.isLetter(ch))
             numLetters++;
       }
       System.out.println();
-      System.out.println
-         ("The number of CHARACTERS in the string is " + stringLength);
-      System.out.println("The number of LETTERS is " + stringLength);
+      System.out.println("The number of CHARACTERS in the string is " + stringLength);
+      System.out.println("The number of LETTERS is " + numLetters);
    }
 }
