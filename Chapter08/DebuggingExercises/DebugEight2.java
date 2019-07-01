32c419a0c0e704feedcca185420563ef588ad8e9
Ray Peng <pengray2015@gmail.com>
2019-07-01 14:10:30 -0700

    Im not sure how to do Debug 1 and 3 and I wasn't sure how to figure out4

--- Chapter08/DebuggingExercises/DebugEight2.java
+++ Chapter08/DebuggingExercises/DebugEight2.java
@@ -3,12 +3,12 @@ public class DebugEight2
 {
    public static void main(String args[])
    {
-      int[] someNums = {4, 17, 22, 8, 35}
+      int[] someNums = {4, 17, 22, 8, 35};
       int tot = 0;
       int x;
-      for(x = 0; x < someNums; ++x)
-        tot = someNums[someNumslength];
-      System.out.println("Sum is  tot);
-      System.out.println("Average is " + tot);
+      for(x = 0; x < someNums.length; ++x)
+        tot += someNums[x];
+      System.out.println("Sum is " + tot);
+      System.out.println("Average is " + tot/5);
    }
 }
