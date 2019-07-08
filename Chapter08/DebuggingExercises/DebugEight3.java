1dcb36cc273332675affc1fae1aeb8cc7db595e9
Ray Peng <pengray2015@gmail.com>
2019-07-08 16:25:56 -0700

    idk when these are from

--- Chapter08/DebuggingExercises/DebugEight3.java
+++ Chapter08/DebuggingExercises/DebugEight3.java
@@ -5,11 +5,11 @@ import javax.swing.*;
 public class DebugEight3
 {
    public static void main(String[] args)
-   { 
+   {
       String entry;
       char[] floorPlans = {'A','B','C','a','b','c'};
       int[] pricesInThousands = {145, 190, 235};
-      char plan;
+      char plan = 'A';
       int x, fp = 99;
       String prompt = "Please select a floor plan\n" +
           "Our floorPlanss are:\n" + "A - Augusta, a ranch\n" +
@@ -17,8 +17,8 @@ public class DebugEight3
           "C - Colonial, a two-story\n" +
           "Enter floorPlans letter";
       entry = JOptionPane.showInputDialog(null, prompt);
-      for(x = 0; x > floorPlans.length; ++x)
-	 if(plan = floorPlans[x])
+      for(x = 0; x < floorPlans.length; ++x)
+	 if(plan == floorPlans[x])
            fp = x;
       if(fp == 99)
         JOptionPane.showMessageDialog(null,
@@ -26,11 +26,10 @@ public class DebugEight3
       else
       {
         if(fp > floorPlans.length)
-           fp = pf - floorPlans.length;
+           fp = fp - floorPlans.length;
         JOptionPane.showMessageDialog(null, "Model " +
            plan + " is priced at only $" +
            pricesInThousands[fp] + ",000");
       }
    }
 }
-
