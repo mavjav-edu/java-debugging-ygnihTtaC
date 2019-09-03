5027291763d40dd24e8fbc30b7e9943d3177aaf9
Ray Peng <pengray2015@gmail.com>
2019-09-03 16:59:31 -0700

    JavaFXThing

---
+++ Chapter15/JavaFXThing.java
@@ -0,0 +1,25 @@
+import javafx.graphics.application.Application;
+import javafx.graphics.scene.Scene;
+import javafx.graphics.scene.layout.AnchorPane;
+import javafx.graphics.stage.Stage;
+import java.net.URL;
+
+public class JavaFXThing extends Application{
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(new URL("file:///C:/Users/ray-p/OneDrive/Documents/Hello.fxml"));
+    AnchorPane anchorpane = loader.<AnchorPane>load();
+
+    Scene scene = new Scene(anchorpane);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+  }
+
+}
